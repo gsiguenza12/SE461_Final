19090792b0fe245f29abdfc4f17a226fae8bcca6
garci1784 <90440339+garci1784@users.noreply.github.com>
2022-05-10 15:26:25 -0700

    Add files via upload

--- Test/BoardPanelTest.java
+++ Test/BoardPanelTest.java
@@ -1,16 +1,36 @@
+import static org.junit.Assert.*;
+
+import java.awt.Graphics;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.psnbtech.BoardPanel;
+import org.psnbtech.SnakeGame;
 
 public class BoardPanelTest {
-    public BoardPanel B;
 
-    @Before
-    public void setUp() throws Exception {
+	@Test
+	public void testClearBoard() {
+		BoardPanel bp = new BoardPanel(null); // no snake game as argument
+		
+		bp.setTile(0, 0, bp.getTile(5,5));
+		
+		bp.clearBoard();
+		
+		assertEquals(bp.getTile(0, 0), null);
+		
+		try
+		{
+			Graphics g = (Graphics) bp.getGraphics();
+			
+			assertNull(g);
+			
+		} finally {
+			
+		}
 
-    }
-
-    @Test
-    public void clearBoardTest() {
-
-    }
+		
+		
+		
+	}		
 }
